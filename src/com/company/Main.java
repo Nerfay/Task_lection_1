bca6bedd2b7956863831a2c77f148274686953f3
Nerfay <antonishen68@gmail.com>
2022-03-15 18:21:57 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cyan = "\033[4;36m";;
+        String blue = "\033[0;34m";
+        String white = "\033[4;37m";
+        String black = "\033[4;30m";
+        String purple = "\033[4;35m";
+        String a = "\u2588";
+        System.out.println(white + a + a + a + a + a+ a + a + a + a + a+black+a+a+white + a + a + a + a + a+ a + a + a + a + a+ a + a + a + a + a+ a + a + a + a + a );
+        System.out.println(white + a + a + a + a + a+ a + a + a + a + a+black+a+blue+a+black+a+white  + a + a + a + a+ a + a + a + a + a+ a + a + a + a + a+ a + a + a + a + a );
+        System.out.println(white + a + a + a + a + a+ a + a + a + a + a+black+a+blue+a+blue+a+black+a+white  + a + a + black+a+a+a+a+a+a+a+white+a+a+a+a+a+a +a+a+a);
+        System.out.println(white + a + a + a + a + a+ a + a + a + a + a+black+a+blue+a+a+a+black  + a + a + blue+a+a+a+a+a+a+a+black+a+a+white+a+a+a+a+a+a +a);
+        System.out.println(white + a + a+ a+ a+black+a+a+white + a + a + a + a+black+a+blue+a+a+a+black  + a  + blue+a+a+a+a+a+a+a+a+a+a+black+a+white+a+a+a+a+a+a );
+        System.out.println(white+a+a+a+black+a+blue+a+a+black+a+white+a+a+a+a+black+a+blue+a+black+a+blue+a+a+a+a+a+black+a+a+a+blue+a+a+a+a+black+a+white+a+a+a+a+a);
+        System.out.println(white+a+a+a+black+a+blue+a+a+a+black+a+white+a+a+a+black+a+a+blue+a+a+a+a+a+black+a+a+cyan+a+a+a+blue+a+a+a+black+a+a+white+a+a+a+a);
+        System.out.println(white+a+a+a+a+black+a+a+blue+a+a+black+a+white+a+a+a+black+a+blue+a+a+a+a+a+black+a+cyan+a+a+a+a+a+blue+a+a+a+black+a+a+white+a+a+a);
+        System.out.println(white+a+a+a+black+a+purple+a+a+black+a+blue+a+a+black+a+a+white+a+black+a+blue+a+a+a+a+a+black+a+cyan+a+a+black+a+a+white+a+cyan+a+blue+a+a+black+a+a+white+a+a+a);
+        System.out.println(white+a+a+a+black+a+purple+a+a+a+black+a+blue+a+a+a+black+a+blue+a+a+a+a+a+a+a+cyan+a+a+black+a+a+a+a+cyan+a+blue+a+black+a+a+white+a+a+a);
+        System.out.println(white+a+a+a+black+a+purple+a+a+a+a+black+a+a+blue+a+a+a+a+a+a+a+a+a+cyan+a+a+black+a+a+a+a+cyan+a+blue+a+a+black+a+white+a+a+a);
+        System.out.println(white+a+a+a+a+black+a+purple+a+a+a+a+a+black+a+blue+a+a+a+a+a+a+a+a+a+cyan+a+black+a+a+a+a+cyan+a+blue+a+black+a+white+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+black+a+purple+a+a+a+a+a+black+a+a+blue+a+a+a+a+a+a+a+a+cyan+a+a+a+a+blue+a+black+a+white+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+black+a+a+purple+a+a+a+a+a+black+a+blue+a+black+a+blue+a+a+a+a+a+a+a+a+black+a+a+white+a+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+black+a+a+a+a+a+a+a+blue+a+black+a+a+a+a+a+a+a+a+white+a+a+a+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+a+a+a+black+a+a+blue+a+a+a+a+black+a+cyan+a+a+a+black+a+white+a+a+a+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+a+a+a+a+a+black+a+cyan+a+black+a+white+a+a+a+a+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+a+a+black+a+blue+a+a+a+black+a+cyan+a+black+a+white+a+a+a+a+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+a+cyan+a+a+black+a+blue+a+a+a+black+a+blue+a+black+a+a+a+white+a+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+a+black+a+a+cyan+a+black+a+a+blue+a+a+black+a+blue+a+black+a+a+blue+a+black+a+white+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+black+a+blue+a+black+a+a+blue+a+a+black+a+blue+a+black+a+blue+a+black+a+blue+a+a+black+a+white+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+a+a+a+black+a+blue+a+a+a+black+a+blue+a+black+a+a+a+blue+a+a+black+a+white+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+a+a+a+a+a+a+a+black+a+blue+a+a+black+a+blue+a+black+a+blue+a+black+a+white+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+black+a+a+blue+a+a+a+black+a+blue+a+a+black+a+blue+a+a+a+a+black+a+blue+a+black+a+white+a+a+a+a+a+a);
+        System.out.println(white+a+a+a+a+a+a+a+a+a+a+a+a+black+a+a+a+a+a+a+a+a+a+a+a+a+a+a+white+a+a+a+a+a+a);
+    }
+}
